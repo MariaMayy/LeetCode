38201719bd8155a0920e997cdebf1a1f3a8d2920
Maria <64471671+MariaMayy@users.noreply.github.com>
2022-01-31 00:26:58 +0300

    Solution #278

--- first_bad_version.cpp
+++ first_bad_version.cpp
@@ -4,15 +4,17 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int iLow = 1;
-        int iHigh = n;
-        int iMid;
+        long int iLow = 1;
+        long int iHigh = n;
+        long int iMid;
         
+        if (n == 1) return 1;
         while (iLow <= iHigh) {
             iMid = (iLow + iHigh) / 2;
             if (!isBadVersion(iMid)) iLow = iMid + 1;
             else iHigh = iMid - 1;
         }
-        return iMid;
+        if (isBadVersion(iLow)) return iLow;
+        else return iHigh;
     }
 };
