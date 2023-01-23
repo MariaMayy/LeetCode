8f9d9bc72402948059d5b762bc0d198d788c4537
Maria <64471671+MariaMayy@users.noreply.github.com>
2023-01-24 01:56:12 +0300

    Solution #7

---
+++ reverse_integer.cpp
@@ -0,0 +1,15 @@
+// Time: O(log(x))
+class Solution {
+public:
+    int reverse(int x) {
+        long iNum = 0;
+        
+        while (x != 0) {
+          iNum = iNum * 10 + (x % 10);
+          x = x / 10;
+        }
+        
+        if (iNum > INT_MAX || iNum < INT_MIN) return 0;
+        else return iNum;
+    }
+};
